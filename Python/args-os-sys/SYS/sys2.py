5d58530cc450b9229021486180cea21f10af4e07
Joska99 <likejoska@gmail.com>
2023-03-13 18:53:41 +0200

    py update

---
+++ Python/args-os-sys/SYS/sys2.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    line = input()
+    while len(line) > 0:
+        print(line.upper())
+        line = input()
---
+++ Python/generator.py
@@ -0,0 +1,22 @@
+from itertools import takewhile
+import sys
+
+
+def primes():
+    ps = []
+    n = 2
+    while True:
+        for p in takewhile(lambda x: x * x <= n, ps):
+            if n % p == 0:
+                break
+        else:
+            yield n
+            ps.append(n)
+        n += 1
+
+
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+
+    for p in takewhile(lambda q: q < n, primes()):
+        print(p)
