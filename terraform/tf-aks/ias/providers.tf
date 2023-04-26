65d7f51458915386fab4d0fb4056ff9745b697d4
joska99 <likejoska@gmail.com>
2023-04-26 12:00:03 +0300

    README

--- terraform/tf-aks/ias/providers.tf
+++ terraform/tf-aks/ias/providers.tf
@@ -6,6 +6,15 @@ terraform {
       version = "3.45.0"
     }
   }
+  ######################
+  # BACKEND FOR .TFSTATE
+  ######################
+  backend "azurerm" {
+    resource_group_name  = "aks-rg"
+    storage_account_name = "sa4tstfstate"
+    container_name       = "tf-state"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
