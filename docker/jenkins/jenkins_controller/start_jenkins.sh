0e98bb27dcadd671cf5eafa6df8646f549b28172
Joseph Kanzaveli <youngyotheb@gmail.com>
2024-03-09 13:02:05 +0200

    update jenkins controller

--- docker/jenkins/jenkins_controller/start_jenkins.sh
+++ docker/jenkins/jenkins_controller/start_jenkins.sh
@@ -3,12 +3,18 @@ docker build -t jenkins .
 
 # Run container image
 # -p - run on port
+# -v - attach persistance storage, the user in container must have access to dir
+# -t - image tag to run
 # -d - detach mode (backgound)
 # --name - chose container name
 # --restart=on-failure - automatically restart container
-# -t - image tag to run
-# -v - attach persistance storage, the user in container must have access to dir
-docker run -p 80:8080 -d -v /home/joska/jenkins_home:/var/jenkins_home --name jenkins --restart=on-failure -t jenkins/jenkins
+docker run \
+    -p 80:8080 \ 
+    -v /home/joska/jenkins_home:/var/jenkins_home \
+    --name jenkins \
+    --restart=on-failure \
+    -t jenkins/jenkins \
+    -d 
 
 #! First time login
 # docker exec jenkins cat /var/jenkins_home/secrets/initialAdminPassword
